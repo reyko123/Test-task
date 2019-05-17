c7bda46310df71d5d1b55ddcade8f95c325d92f5
R3I <reyko123@mail.ru>
2019-05-17 09:51:22 +0500

    123

--- viev/glitch.php
+++ viev/glitch.php
@@ -1,6 +1,6 @@
 <! DOCTYPE html>
 <head>
-  
+
 <style type="text/css">
 @import "compass/css3";
 
@@ -18,7 +18,7 @@ svg{
   height: 120px;
   display: block;
   position: relative;
-  overflow: hidden; 
+  overflow: hidden;
   margin: 0 auto;
   background: black;
 }
@@ -28,7 +28,7 @@ svg{
 </head>
 <body>
 
-<svg version="1.1" id="Ebene_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" 
+<svg version="1.1" id="Ebene_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
    width="600px" height="100px" viewBox="0 0 600 100">
 <style type="text/css">
 
@@ -62,58 +62,58 @@ svg{
         <feMergeNode in = "comp1" />
         <feMergeNode in = "off1b" />
 
-        <animate 
-          attributeName="y" 
+        <animate
+          attributeName="y"
             id = "y"
             dur ="4s"
-            
+
             values = '104px; 104px; 30px; 105px; 30px; 2px; 2px; 50px; 40px; 105px; 105px; 20px; 6ßpx; 40px; 104px; 40px; 70px; 10px; 30px; 104px; 102px'
 
             keyTimes = '0; 0.362; 0.368; 0.421; 0.440; 0.477; 0.518; 0.564; 0.593; 0.613; 0.644; 0.693; 0.721; 0.736; 0.772; 0.818; 0.844; 0.894; 0.925; 0.939; 1'
 
             repeatCount = "indefinite" />
- 
-        <animate attributeName="height" 
-            id = "h" 
+
+        <animate attributeName="height"
+            id = "h"
             dur ="4s"
-            
+
             values = '10px; 0px; 10px; 30px; 50px; 0px; 10px; 0px; 0px; 0px; 10px; 50px; 40px; 0px; 0px; 0px; 40px; 30px; 10px; 0px; 50px'
 
             keyTimes = '0; 0.362; 0.368; 0.421; 0.440; 0.477; 0.518; 0.564; 0.593; 0.613; 0.644; 0.693; 0.721; 0.736; 0.772; 0.818; 0.844; 0.894; 0.925; 0.939; 1'
 
             repeatCount = "indefinite" />
         </feMerge>
-      
+
 
       <feMerge x="0" width="100%" y="60px" height="65px" result="merge2">
         <feMergeNode in = "black" />
         <feMergeNode in = "comp2" />
         <feMergeNode in = "off2b" />
 
-        <animate attributeName="y" 
+        <animate attributeName="y"
             id = "y"
             dur ="4s"
-            values = '103px; 104px; 69px; 53px; 42px; 104px; 78px; 89px; 96px; 100px; 67px; 50px; 96px; 66px; 88px; 42px; 13px; 100px; 100px; 104px;' 
+            values = '103px; 104px; 69px; 53px; 42px; 104px; 78px; 89px; 96px; 100px; 67px; 50px; 96px; 66px; 88px; 42px; 13px; 100px; 100px; 104px;'
 
             keyTimes = '0; 0.055; 0.100; 0.125; 0.159; 0.182; 0.202; 0.236; 0.268; 0.326; 0.357; 0.400; 0.408; 0.461; 0.493; 0.513; 0.548; 0.577; 0.613; 1'
 
             repeatCount = "indefinite" />
- 
-        <animate attributeName="height" 
+
+        <animate attributeName="height"
             id = "h"
             dur = "4s"
-          
+
           values = '0px; 0px; 0px; 16px; 16px; 12px; 12px; 0px; 0px; 5px; 10px; 22px; 33px; 11px; 0px; 0px; 10px'
 
             keyTimes = '0; 0.055; 0.100; 0.125; 0.159; 0.182; 0.202; 0.236; 0.268; 0.326; 0.357; 0.400; 0.408; 0.461; 0.493; 0.513;  1'
-             
+
             repeatCount = "indefinite" />
         </feMerge>
-      
-      <feMerge>
-        <feMergeNode in="SourceGraphic" />  
 
-        <feMergeNode in="merge1" /> 
+      <feMerge>
+        <feMergeNode in="SourceGraphic" />
+
+        <feMergeNode in="merge1" />
       <feMergeNode in="merge2" />
 
         </feMerge>
@@ -122,7 +122,7 @@ svg{
   </defs>
 
 <g>
-  <text x="50" y="100">Проход для Ботьки</text>
+  <text x="50" y="100">Test</text>
 </g>
 </svg>
 </body>
